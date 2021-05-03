0318bd91a3a19d85369ce08ac94a41d0a294769c
Terina227227 <t.zourkova@gmail.com>
2021-05-03 10:51:04 +0200

    cviceni fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,14 @@
+def recursive_nth_fibo(n):
+    if n<=1:
+        return 1
+
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
 
 def main():
-    pass
+    kolik_prvku = recursive_nth_fibo(int(input("zadej kolik chces vypsat cisel: ")))
+    posloupnost = [recursive_nth_fibo(n) for n in range(kolik_prvku)]
+    print(posloupnost)
 
 
 if __name__ == '__main__':
